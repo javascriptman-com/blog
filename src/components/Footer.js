cf40f4dea8a4a8c329099d606568d090578a3605
Himalay <himalay@innovatetech.co>
2019-10-28 00:37:17 +0545

    Update branding and hide unwanted links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,12 +34,12 @@ const Footer = class extends React.Component {
                         About
                       </Link>
                     </li>
-                    <li>
+                    {/* <li>
                       <Link className="navbar-item" to="/products">
                         Products
                       </Link>
-                    </li>
-                    <li>
+                    </li> */}
+                    {/* <li>
                       <Link className="navbar-item" to="/contact/examples">
                         Form Examples
                       </Link>
@@ -53,7 +53,7 @@ const Footer = class extends React.Component {
                       >
                         Admin
                       </a>
-                    </li>
+                    </li> */}
                   </ul>
                 </section>
               </div>
